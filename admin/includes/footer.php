df8bf372a029ff20b0e24486e310ca1a2bd7952b
fernandozdv <fernandozdv@gmail.com>
2017-07-26 06:36:49 -0500

    tutorial 8

---
+++ admin/includes/footer.php
@@ -0,0 +1,7 @@
+</div>
+
+<footer class="col-md-12 text-center" id="footer">&copy; Copyright 2017 Fernando Zuñe</footer>
+
+
+</body>
+</html>
---
+++ admin/includes/navigation.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container">
+    <a href="/tutorial/phpProject2/admin/index.php" class="navbar-brand">Admin tiendita</a>
+    <ul class="nav navbar-nav">
+      <!--Menu items-->
+      <li><a href="brands.php">Brands</a></li>
+      <!-- <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><?php echo $parent['category']; ?> <span class="caret"></span></a>
+        <ul class="dropdown-menu" role="menu">
+            <li><a href="#"></a></li>
+        </ul>
+      </li> -->
+    </ul>
+  </div>
+</nav>
