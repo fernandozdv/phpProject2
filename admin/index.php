15a7909b83eb2a3cb7a3ad7f13ee8eafa3e74d55
fernandozdv <fernandozdv@gmail.com>
2017-08-16 01:07:18 -0500

    tutorial 39

--- admin/index.php
+++ admin/index.php
@@ -43,4 +43,90 @@ include 'includes/navigation.php';
       </table>
     </div>
   </div>
+  <div class="row">
+    <!-- Ventas del mes -->
+    <?php
+      $thisYr=date("Y");
+      $lastYr=$thisYr-1;
+      $thisYrQ=$db->query("SELECT grand_total,txn_date FROM transactions WHERE YEAR(txn_date)='$thisYr'");
+      $lastYrQ=$db->query("SELECT grand_total,txn_date FROM transactions WHERE YEAR(txn_date)='$lastYr'");
+      //Ventas de este año
+      $current=array();
+      //Ventas del año anterior
+      $last=array();
+      $currentTotal=0;
+      $lastTotal=0;
+      //PARA ESTE MES
+      while($x=mysqli_fetch_assoc($thisYrQ))
+      {
+        //Mes y darle formato de mes de la fecha de compra
+        //Retorna un entero!!!
+        $month=idate('m',strtotime($x['txn_date']));
+        //Verifica si existe la clave $month en $current
+        if(!array_key_exists($month,$current))
+        {
+          var_dump($current);
+          //Agrega el mes como llave y almacena el total
+          $current[$month]=$x['grand_total'];
+        }else{
+          echo ' no'.$month;
+          //Si ya existe el mes como llave, suma el total para
+          //calcular el total por mes
+          $current[$month]+=$x['grand_total'];
+        }
+        $currentTotal+=$x['grand_total'];
+      }
+
+      //PARA EL MES PASADO
+      while($y=mysqli_fetch_assoc($lastYrQ))
+      {
+        //Mes y darle formato de mes de la fecha de compra
+        $month=idate('m',strtotime($y['txn_date']));
+        //Verifica si existe la clave $month en $current
+        if(!array_key_exists($month,$current))
+        {
+          //Agrega el mes como llave y almacena el total
+          $last[$month]=$y['grand_total'];
+        }else{
+          //Si ya existe el mes como llave, suma el total para
+          //calcular el total por mes
+          $last[$month]+=$y['grand_total'];
+        }
+        $lastTotal+=$x['grand_total'];
+      }
+
+     ?>
+    <div class="col-md-4">
+      <h3 class="text-center">Ventas del mes</h3>
+      <table class="table table-bordered table-condensed table-striped">
+        <thead>
+          <th></th>
+          <th><?=$lastYr;?></th>
+          <th><?=$thisYr;?></th>
+        </thead>
+        <tbody>
+          <?php for($i=1;$i<=12;$i++):
+            //Dar formato de mes desde 1,2...hasta 12
+            $dt=DateTime::createFromFormat('m',$i);
+            ?>
+            <tr>
+              <!-- Se mostrará como mes en texto completo en inglés -->
+              <td><?=$dt->format("F");?></td>
+              <td><?=((array_key_exists($i,$last))?money($last[$i]):money(0));?></td>
+              <td><?=((array_key_exists($i,$current))?money($current[$i]):money(0));?></td>
+            </tr>
+          <?php endfor; ?>
+            <tr>
+              <td>Total</td>
+              <td><?=$lastTotal;?></td>
+              <td><?=$currentTotal;?></td>
+            </tr>
+        </tbody>
+      </table>
+    </div>
+    <!-- Inventario -->
+    <div class="col-md-8">
+
+    </div>
+  </div>
  <?php include 'includes/footer.php'; ?>
